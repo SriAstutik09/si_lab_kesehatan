5e9bb4d399328dd77d7d2c9701c67f97c7abdef4
Sriastutik09 <alamiastutik@gmail.com>
2026-07-22 14:35:54 +0700

    perbaikan error

--- aslab/views.py
+++ aslab/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_split, redirect
+# SAKLAR BENAR (perbaikan):
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from core_auth.models import Peminjaman
