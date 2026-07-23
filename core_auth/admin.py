4de8c966a437fd7cedbc4eae257507c2aa36378e
Sriastutik09 <alamiastutik@gmail.com>
2026-07-23 17:03:32 +0700

    cek database admin

--- core_auth/admin.py
+++ core_auth/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# Panggil model/tabel dari file models.py
+from .models import *  
 
-# Register your models here.
+# Kode di bawah ini berguna untuk mendaftarkan SEMUA tabel agar muncul di admin
+from django.apps import apps
+
+app = apps.get_app_config('core_auth')
+
+for model_name, model in app.models.items():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
